1e03a5bf11bf0d92753efc2913d1a138fa8ab5b7
forloop <yendheakshayd@gmail.com>
2023-02-03 11:24:59 +0530

    UserDefault

--- UserDefault/ViewController.swift
+++ UserDefault/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    public var history = [String]()
 
+    @IBOutlet weak var num1TextField: UITextField!
+    @IBOutlet weak var num2TextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
